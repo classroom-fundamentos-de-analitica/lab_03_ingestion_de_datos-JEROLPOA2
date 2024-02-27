5f865f038605df7759cc04e6d2f368a578965a06
JEROLPOA2 <jledesmap@unal.edu.co>
2024-02-27 18:44:08 -0500

    Update

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,86 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
+import re
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
 
-    return df
+    with open('clusters_report.txt', 'r') as file:
+        
+        # PROCESAMIENTO DE ENCABEZADOS
+
+        header = []
+
+        for line in file:
+            
+            if re.match(r'-+', line):
+                break
+
+            else:
+
+                titles = line.lower()
+                titles = re.sub(r'\S\s\S', lambda x: x.group(0)[0] + '-' + x.group(0)[-1], titles)
+
+                header.append([(x, y.start()) 
+                               for x in titles.split()
+                               for y in re.finditer(r'\b' + re.escape(x) + r'\b', titles)])
+                
+        
+        header = [list(set(x)) for x in header]
+        headerdict = {}
+
+        for i in header:
+
+            for item, key in i:
+
+                if key in headerdict:
+                    headerdict[key] = headerdict[key] + "-" + item
+                
+                else:
+                    headerdict[key] = item
+        
+
+        headerdict = sorted(headerdict.items())
+        header = [x[1] for x in headerdict]
+
+        df = pd.DataFrame()
+        
+        for i in header:
+            df[i] = []
+
+        # PROCESAMIENTO DE ENTRADAS
+        while True:
+
+            data = []
+
+            for line in file:
+                
+                lines = [x for x in line.strip().replace("%", "").split(sep=" ") if x != ""]
+
+                if lines == []:
+                    break
+
+                else: 
+                    data += lines
+
+            if len(data) == 0:
+                break
+
+            entry_1 = int(data[0])
+            entry_2 = int(data[1])
+            entry_3 = float(data[2].replace(",", "."))
+            entry_4 = ' '.join(map(str, data[3:]))
+
+            row = {header[0]: entry_1, 
+                header[1]: entry_2, 
+                header[2]: entry_3, 
+                header[3]: entry_4,}
+            
+            df = df.append(row, ignore_index=True)
+        
+        return df
+        
+ingest_data()
+
+
